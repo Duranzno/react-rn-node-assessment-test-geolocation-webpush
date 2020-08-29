2f64ab04fdb2eeb841053baa1aaf5bf8396642ce
Alejandro D <aledurax@gmail.com>
2020-08-29 10:59:14 -0400

    ✨ Add Step 1 of Webapp, select

--- packages/webapp/src/components/Select/Select.component.tsx
+++ packages/webapp/src/components/Select/Select.component.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
-
-// interface Props {}
-
-export const Select = () => {
-  return <div></div>
+import Select from 'react-select'
+interface Props {
+  onChange: Function
+  options: string[]
 }
+type OptionType = { value: string; label: string }
+const parser = (data: string[]): OptionType[] =>
+  data.map((v) => ({ value: v, label: v }))
+const unparser = (data: OptionType[]) => data.map(({ value }) => value)
+export const Selector = ({ options, onChange }: Props) => (
+  <Select
+    isMulti
+    name="colors"
+    options={parser(options)}
+    onChange={(v) => {
+      //@ts-ignore
+      onChange(unparser(v))
+    }}
+    className="basic-multi-select"
+    classNamePrefix="select"
+  />
+)
