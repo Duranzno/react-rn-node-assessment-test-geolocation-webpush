5acb1ee9e22cb16f7c3aaa29585ac89b896b95c4
Alejandro D <aledurax@gmail.com>
2020-08-29 14:35:03 -0400

    ✨ Basic MapMarker Added

--- packages/webapp/src/components/MapMarker/MapMarker.component.tsx
+++ packages/webapp/src/components/MapMarker/MapMarker.component.tsx
@@ -1,11 +1,56 @@
+//@ts-nocheck
 import React from 'react'
+import { compose, withStateHandlers } from 'recompose'
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+} from 'react-google-maps'
 
-// interface Props {
+const Map = compose(
+  withStateHandlers(
+    () => ({
+      isMarkerShown: false,
+      markerPosition: null,
+    }),
+    {
+      onMapClick: ({ isMarkerShown }) => (e) => {
+        console.log(JSON.stringify({ isMarkerShown, e }))
+        return {
+          markerPosition: e.latLng,
+          isMarkerShown: true,
+        }
+      },
+    }
+  ),
+  withScriptjs,
+  withGoogleMap
+)((props) => (
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    onClick={props.onMapClick}
+  >
+    {props.isMarkerShown && <Marker position={props.markerPosition} />}
+  </GoogleMap>
+))
 
-// }
-
-export const MapMarker = () =>
-  // props: Props
-  {
-    return <div></div>
+export class MapMarker extends React.Component {
+  constructor(props) {
+    super(props)
   }
+
+  render() {
+    return (
+      <div style={{ height: '100%' }}>
+        <Map
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_KEY}`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
+    )
+  }
+}
